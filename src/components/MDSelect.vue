e069c189233e840f24eefce9b311d85b90c38c81
Jeremiah Poisson <jpoisson@mythicaldev.com>
2023-04-02 12:54:53 -0400

    Fix incorrect tailwind style

--- src/components/MDSelect.vue
+++ src/components/MDSelect.vue
@@ -24,7 +24,7 @@
             @after-leave="query = ''"
           >
             <ComboboxOptions
-              class="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm z-100"
+              class="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm z-50"
             >
               <div
                 v-if="filteredOptions.length === 0 && query !== ''"
