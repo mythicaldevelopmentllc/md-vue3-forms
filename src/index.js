a53e1dad71d8e90e8771c5f0685073a769efbfcc
Jeremiah Poisson <jpoisson@mythicaldev.com>
2023-04-04 14:58:59 -0400

    Export only the components

--- src/index.js
+++ src/index.js
@@ -12,24 +12,6 @@ import MDSelect from './components/MDSelect.vue';
 import MDText from './components/MDText.vue';
 import MDTextArea from './components/MDTextArea.vue';
 
-export default {
-  install: (app) => {
-    app.component('MDForm', MDForm);
-    app.component('MDCheckbox', MDCheckbox);
-    app.component('MDDatePicker', MDDatePicker);
-    app.component('MDDraggablePicker', MDDraggablePicker);
-    app.component('MDDropZone', MDDropZone);
-    app.component('MDError', MDError);
-    app.component('MDGroup', MDGroup);
-    app.component('MDHelpBlock', MDHelpBlock);
-    app.component('MDInput', MDInput);
-    app.component('MDLabel', MDLabel);
-    app.component('MDSelect', MDSelect);
-    app.component('MDText', MDText);
-    app.component('MDTextArea', MDTextArea);
-  },
-};
-
 export {
   MDForm,
   MDCheckbox,
