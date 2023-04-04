9b14f6df4cc75e83fdd1626f45e260ce458f4d7d
Jeremiah Poisson <jpoisson@mythicaldev.com>
2023-04-04 15:01:04 -0400

    Update draggablet to use sortablejs directly
    Removes the dependency on vue.draggable@next

--- src/components/MDDraggablePicker.vue
+++ src/components/MDDraggablePicker.vue
@@ -2,87 +2,75 @@
   <MDLabel v-if="hasLabel" :id="id" :label="label" :required="required" />
   <MDError v-if="error !== ''">{{ error }}</MDError>
   <div class="flex">
-    <div class="w-full mr-6 border border-gray-300 rounded-md p-4 bg-gray-200">
+    <div class="flex flex-col w-full mr-6 border border-gray-300 rounded-md p-4 bg-gray-200">
       <span>{{ availableLabel }}</span>
-      <Draggable
-        group="draggableOption"
-        :list="props.modelValue.available"
-        itemKey="id"
-        :animation="200"
-        @end="onEnd"
-        ghostClass="moving-card"
-      >
-        <template #header>
-          <MDHelpBlock class="pb-4">Drag here to remove an option</MDHelpBlock>
-        </template>
-
-        <template #item="{ element, index }">
-          <div class="p-4 mb-3 items-center bg-white shadow rounded-lg cursor-move" @click="assign(index)">
-            {{ element.name }}
-          </div>
-        </template>
-
-        <template #footer>
-          <div class="flex justify-end">
-            <button
-              v-if="props.modelValue.available.length > 0"
-              type="button"
-              class="px-6 py-2.5 bg-sky-700 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-sky-900 hover:shadow-lg transition duration-150 ease-in-out"
-              @click="assignAll"
-            >
-              Assign All
-            </button>
-          </div>
-        </template>
-      </Draggable>
+      <MDHelpBlock class="pb-4">Drag here to add an option</MDHelpBlock>
+      <component ref="availableContainerRef" :is="'div'" class="grow" :key="containerKey">
+        <div
+          v-for="(item, index) of props.modelValue.available"
+          :key="index"
+          :data-id="item.id"
+          class="p-4 mb-3 items-center bg-white shadow rounded-lg cursor-move"
+          @click="assign(index)"
+        >
+          {{ item.name }}
+        </div>
+      </component>
+      <div class="flex justify-end">
+        <button
+          v-if="props.modelValue.available.length > 0"
+          type="button"
+          class="px-6 py-2.5 bg-sky-700 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-sky-900 hover:shadow-lg transition duration-150 ease-in-out"
+          @click="assignAll"
+        >
+          Assign All
+        </button>
+      </div>
     </div>
 
-    <div class="w-full mr-6 border border-gray-300 rounded-md p-4 bg-gray-200">
+    <div class="flex flex-col w-full mr-6 border border-gray-300 rounded-md p-4 bg-gray-200">
       <span>{{ selectedLabel }}</span>
-      <Draggable
-        group="draggableOption"
-        :list="props.modelValue.selected"
-        itemKey="id"
-        :animation="200"
-        @end="onEnd"
-        ghostClass="moving-card"
-      >
-        <template #header>
-          <MDHelpBlock class="pb-4">Drag here to select an option</MDHelpBlock>
-        </template>
-
-        <template #item="{ element, index }">
-          <div class="p-4 mb-3 items-center bg-white shadow rounded-lg cursor-move" @click="remove(index)">
-            {{ element.name }}
-          </div>
-        </template>
-
-        <template #footer>
-          <div class="flex justify-end">
-            <button
-              v-if="props.modelValue.selected.length > 0"
-              type="button"
-              class="px-6 py-2.5 bg-sky-700 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-sky-900 hover:shadow-lg transition duration-150 ease-in-out"
-              @click="removeAll"
-            >
-              Remove All
-            </button>
-          </div>
-        </template>
-      </Draggable>
+      <MDHelpBlock class="pb-4">Drag here to add an option</MDHelpBlock>
+      <component ref="selectedContainerRef" :is="'div'" class="grow" :key="containerKey">
+        <div
+          v-for="(item, index) of props.modelValue.selected"
+          :key="index"
+          :data-id="item.id"
+          class="p-4 mb-3 items-center bg-white shadow rounded-lg cursor-move"
+          @click="remove(index)"
+        >
+          {{ item.name }}
+        </div>
+      </component>
+      <div class="flex justify-end">
+        <button
+          v-if="props.modelValue.selected.length > 0"
+          type="button"
+          class="px-6 py-2.5 bg-sky-700 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-sky-900 hover:shadow-lg transition duration-150 ease-in-out"
+          @click="removeAll"
+        >
+          Remove All
+        </button>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup>
-  import { computed } from 'vue';
-  import Draggable from 'vuedraggable';
+  import { ref, onUnmounted, watch, computed } from 'vue';
+  import Sortable from 'sortablejs';
   import MDLabel from './MDLabel.vue';
   import MDHelpBlock from './MDHelpBlock.vue';
   import MDError from './MDError.vue';
 
+  const availableContainerRef = ref(null);
+  const selectedContainerRef = ref(null);
+  const availableSortable = ref(null);
+  const selectedSortable = ref(null);
+  const containerKey = ref(1);
+
   // eslint-disable-next-line no-undef
-  const emit = defineEmits(['onDragComplete', 'onAllAssigned', 'onAllRemoved']);
+  const emit = defineEmits(['update:modelValue', 'onUpdate']);
   // eslint-disable-next-line no-undef
   const props = defineProps({
     label: {
@@ -124,39 +112,104 @@
     },
   });
 
+  onUnmounted(() => {
+    if (availableSortable.value) {
+      availableSortable.value.destroy();
+      availableContainerRef.value = null;
+      availableSortable.value = null;
+    }
+    if (selectedSortable.value) {
+      selectedSortable.value.destroy();
+      selectedContainerRef.value = null;
+      selectedSortable.value = null;
+    }
+  });
+
+  watch(availableContainerRef, (newDraggable) => {
+    if (newDraggable) {
+      buildAvailableSortable();
+    }
+  });
+
+  watch(selectedContainerRef, (newDraggable) => {
+    if (newDraggable) {
+      buildSelectedSortable();
+    }
+  });
+
+  const buildAvailableSortable = () => {
+    availableSortable.value = new Sortable(availableContainerRef.value, {
+      group: 'test',
+      sort: false,
+      onEnd: (event) => {
+        assign(event.oldIndex);
+      },
+    });
+  };
+
+  const buildSelectedSortable = () => {
+    selectedSortable.value = new Sortable(selectedContainerRef.value, {
+      group: 'test',
+      sort: false,
+      onEnd: (event) => {
+        remove(event.oldIndex);
+      },
+    });
+  };
+
   const assign = (index) => {
-    const company = props.modelValue.available[index];
-    props.modelValue.selected.push(company);
-    props.modelValue.available.splice(index, 1);
-    emit('onDragComplete');
+    const [available, selected] = getLists();
+    const item = available[index];
+    selected.push(item);
+    available.splice(index, 1);
+    updateModelValue(available, selected);
+    containerKey.value++;
   };
 
   const remove = (index) => {
-    const company = props.modelValue.selected[index];
-    props.modelValue.available.push(company);
-    props.modelValue.selected.splice(index, 1);
-    emit('onDragComplete');
+    const [available, selected] = getLists();
+    const item = selected[index];
+    available.push(item);
+    selected.splice(index, 1);
+    updateModelValue(available, selected);
+    containerKey.value++;
   };
 
   const assignAll = () => {
-    // Move all back to available if any.
-    removeAll();
-    // Make all available assigned and clear the available list.
-    props.modelValue.selected = props.modelValue.available;
-    props.modelValue.available = [];
-    emit('onAllAssigned');
+    let [available, selected] = getLists();
+    const allAvailable = available.concat(selected);
+    selected = [...allAvailable];
+    available = [];
+    updateModelValue(available, selected);
+    containerKey.value++;
   };
 
   const removeAll = () => {
-    props.modelValue.selected.forEach((company) => {
-      props.modelValue.available.push(company);
-    });
-    props.modelValue.selected = [];
-    emit('onAllRemoved');
+    let [available, selected] = getLists();
+    const allAvailable = available.concat(selected);
+    selected = [];
+    updateModelValue(allAvailable, selected);
+    containerKey.value++;
   };
 
-  const onEnd = () => {
-    emit('onDragComplete');
+  const getLists = () => {
+    return [getAvailableItems(), getSelectedItems()];
+  };
+
+  const getAvailableItems = () => {
+    return [...props.modelValue.available];
+  };
+
+  const getSelectedItems = () => {
+    return [...props.modelValue.selected];
+  };
+
+  const updateModelValue = (available, selected) => {
+    emit('update:modelValue', {
+      available: available,
+      selected: selected,
+    });
+    emit('onUpdate');
   };
 
   const hasLabel = computed(() => {
